03b1e7a1aaeec13e4404be1f75e488691bf7cc00
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-11-02 12:19:32 -0700

    Add files via upload
    Master Error Handling: Using the throw Statement in JavaScript!

---
+++ script.js
@@ -0,0 +1,26 @@
+// chapter 88 - Exception : throws
+
+ function checkPassword() {
+     try {
+     var pass = document.getElementById("f1").value;
+     if (pass.length < 8) {
+     throw "Please enter at least 8 characters.";
+     }
+     if (pass.indexOf(" ") !== -1) {
+     throw "No spaces in the password, please.";
+     }
+     var numberSomewhere = false;
+     for (var i = 0; i < pass.length; i++) {
+     if (isNaN(pass[i, i+1]) === false) {
+     numberSomewhere = true;
+     break;
+     }
+     }
+     if (numberSomewhere === false) {
+     throw "Include at least 1 number.";
+     }
+     }
+     catch(err) {
+     alert(err);
+     }
+     }
